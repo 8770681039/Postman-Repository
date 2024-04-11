85373492455cf03363364c8f011994a8b988875a
8770681039 <DELL@192.168.10.34>
2024-04-11 09:24:25 +0530

    hii this is reat Assured project

---
+++ src/main/java/Request/RestAssured/App.java
@@ -0,0 +1,13 @@
+package Request.RestAssured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
